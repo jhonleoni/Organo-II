9436532fc8108e3cfb26fe1946b600e830e6b4b3
Jhony Almeida <106479072+jhonleoni@users.noreply.github.com>
2024-02-24 11:09:31 -0400

    refactoring campo component

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -14,6 +14,9 @@ const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
         <div className="rodape">
             <h4>{colaborador.nome}</h4>
             <h5>{colaborador.cargo}</h5>
+            <div className="favoritar">
+                {colaborador.favorito ? 'favorito' : 'nao favorito'}
+            </div>
         </div>
     </div>)
 }
